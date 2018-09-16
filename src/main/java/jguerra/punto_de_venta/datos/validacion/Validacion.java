600c6fe172142d1403362e774815cca9c1acbda8
Juan Guerra <juanguerra97@protonmail.com>
2018-09-16 13:48:48 -0600

    Clase con campos y metodos relacionados con la validacion de los datos del modelo

---
+++ src/main/java/jguerra/punto_de_venta/datos/validacion/Validacion.java
@@ -0,0 +1,46 @@
+package jguerra.punto_de_venta.datos.validacion;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public final class Validacion {
+	
+	public static final Pattern TELEFONO = Pattern.compile("^\\d{8}$");
+	
+	public static final Pattern NIT = Pattern.compile("^(\\d)+-([\\dk])$", Pattern.CASE_INSENSITIVE);
+	
+	/**
+	 * Metodo para saber si un NIT es valido o no
+	 * @param nit cadena con el NIT a validar, no debe ser null
+	 * @return true si el NIT es valido, false si no
+	 */
+	public static boolean validarNit(final String nit) {
+		assert nit != null;
+		
+		Matcher matcher = NIT.matcher(nit);
+		
+		if(matcher.find()) {
+			String leftPart = matcher.group(1);// parte izquierda del guion(sólo números)
+			String rightPart = matcher.group(2);// parte derecha del guion(número o letra 'k')
+			
+			int digitoVerificador = rightPart.equalsIgnoreCase("k") ? 10 : Integer.parseInt(rightPart);
+			
+			long leftNum = Long.parseLong(leftPart);
+			int suma = 0, pos = 2;
+			while(leftNum > 9) {
+				int digito = (int) (leftNum % 10);
+				suma += digito * pos;
+				leftNum /= 10;
+				++pos;
+			}
+			suma += leftNum * pos;
+			
+			int resultado = (11 - suma % 11) % 11;
+			
+			return resultado == digitoVerificador && resultado != 0;
+		}
+		
+		return false;
+	}
+
+}
