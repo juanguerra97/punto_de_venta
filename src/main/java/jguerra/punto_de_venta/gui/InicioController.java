2a4dc6df146ffe32f44e3c2e0f43eaf132cb9a10
Juan Guerra <juanguerra97@protonmail.com>
2018-09-26 11:23:50 -0600

    GUI de inicio del programa

---
+++ src/main/java/jguerra/punto_de_venta/gui/InicioController.java
@@ -0,0 +1,26 @@
+package jguerra.punto_de_venta.gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Tab;
+
+public class InicioController {
+
+	@FXML
+    private Tab tabProductos;
+
+    @FXML
+    private Tab tabSucursales;
+
+    @FXML
+    private Tab tabClientes;
+
+    @FXML
+    private Tab tabProveedores;
+
+    @FXML
+    private Tab tabCompras;
+
+    @FXML
+    private Tab tabVentas;
+	
+}
