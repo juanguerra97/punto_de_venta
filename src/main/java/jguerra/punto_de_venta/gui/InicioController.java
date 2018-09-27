1cdb711c7b1bdaa7ef98d6d52325faf656ba3e37
Juan Guerra <juanguerra97@protonmail.com>
2018-09-27 10:44:45 -0600

    Se carga la interfaz de sucursales en la pestaña correspondiente

--- src/main/java/jguerra/punto_de_venta/gui/InicioController.java
+++ src/main/java/jguerra/punto_de_venta/gui/InicioController.java
@@ -1,6 +1,9 @@
 package jguerra.punto_de_venta.gui;
 
+import java.io.IOException;
+
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Tab;
 
 public class InicioController {
@@ -22,5 +25,17 @@ public class InicioController {
 
     @FXML
     private Tab tabVentas;
+    
+    @FXML
+    private void initialize() {
+    	
+    	try {
+			tabSucursales.setContent(FXMLLoader.load(getClass().getResource("/fxml/sucursales/Sucursales.fxml")));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    	
+    }
+    
 	
 }
