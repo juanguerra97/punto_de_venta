aa70f5d6dedcf02c0a93e0fa801dc407bf0bb3ad
Juan Guerra <juanguerra97@protonmail.com>
2018-09-30 10:11:10 -0600

    Se carga la GUI de productos

--- src/main/java/jguerra/punto_de_venta/gui/InicioController.java
+++ src/main/java/jguerra/punto_de_venta/gui/InicioController.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Tab;
+import jguerra.punto_de_venta.gui.productos.ProductosController;
 
 public class InicioController {
 
@@ -26,12 +27,19 @@ public class InicioController {
     @FXML
     private Tab tabVentas;
     
+    private ProductosController controllerProductos;
+    
     @FXML
     private void initialize() {
     	
     	try {
 			tabSucursales.setContent(FXMLLoader.load(getClass().getResource("/fxml/sucursales/Sucursales.fxml")));
-		} catch (IOException e) {
+			
+			FXMLLoader loaderProd = new FXMLLoader(getClass().getResource("/fxml/productos/Productos.fxml"));
+			tabProductos.setContent(loaderProd.load());
+			controllerProductos = loaderProd.getController();
+			
+    	} catch (IOException e) {
 			e.printStackTrace();
 		}
     	
