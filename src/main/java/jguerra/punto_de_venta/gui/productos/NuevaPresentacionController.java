a04f3c47662ed706d9a5d8f1b93c1f516be3b2d1
Juan Guerra <juanguerra97@protonmail.com>
2018-09-30 14:49:00 -0600

    GUI para ingresar nuevas presentaciones de productos

---
+++ src/main/java/jguerra/punto_de_venta/gui/productos/NuevaPresentacionController.java
@@ -0,0 +1,136 @@
+package jguerra.punto_de_venta.gui.productos;
+
+import java.math.BigDecimal;
+import java.sql.SQLException;
+import java.util.Optional;
+
+import org.controlsfx.control.textfield.CustomTextField;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import jguerra.punto_de_venta.datos.dao.oracle.DAOManager;
+import jguerra.punto_de_venta.datos.modelo.Presentacion;
+import jguerra.punto_de_venta.datos.modelo.Producto;
+import jguerra.punto_de_venta.gui.Main;
+
+public class NuevaPresentacionController {
+	
+	@FXML
+    private CustomTextField fieldNombre;
+
+    @FXML
+    private CustomTextField fieldCosto;
+
+    @FXML
+    private CustomTextField fieldPrecio;
+
+    @FXML
+    private Button btnCancelar;
+
+    @FXML
+    private Button btnGuardar;
+    
+    private DAOManager manager;
+    
+    private Producto producto = null;
+    private Presentacion presentacion = null;
+    
+    public void reset() {
+    	
+    	producto = null;
+    	fieldNombre.clear();
+    	fieldCosto.clear();
+    	fieldPrecio.clear();
+    	fieldNombre.requestFocus();
+    	
+    }
+    
+    public Optional<Presentacion> getPresentacion() {
+    	if(presentacion == null)
+    		return Optional.empty();
+    	Optional<Presentacion> opt = Optional.of(presentacion);
+    	presentacion = null;
+    	return opt;
+    }
+    
+    public void setProducto(final Producto producto) {
+    	assert producto != null;
+    	this.producto = producto;
+    }
+    
+    private boolean valoresCostoPrecioInvalidos() {
+    	boolean costoInvalido = false;
+    	boolean precioInvalido = false;
+    	boolean invalidos = false;
+    	
+    	BigDecimal costo = null;
+    	BigDecimal precio = null;
+    	
+    	try {
+    		costo = new BigDecimal(fieldCosto.getText().trim());
+    	}catch(NumberFormatException ex) {
+    		costoInvalido = true;
+    	}
+    	
+    	try {
+    		precio = new BigDecimal(fieldPrecio.getText().trim());
+    	}catch(NumberFormatException ex) {
+    		precioInvalido = true;
+    	}
+    	invalidos = (costo != null && precio != null && costo.compareTo(precio) > 0);
+    	return costoInvalido || precioInvalido || invalidos;
+    }
+    
+    @FXML
+    private void initialize() {
+    	
+    	manager = DAOManager.instance();
+    	
+    	fieldNombre.textProperty().addListener((observable,oldText,newText)->{
+    		btnGuardar.setDisable(newText.trim().isEmpty() ||
+    				valoresCostoPrecioInvalidos());
+    	});
+    	
+    	fieldCosto.textProperty().addListener((observable,oldText,newText)->{
+    		btnGuardar.setDisable(fieldNombre.getText().trim().isEmpty() ||
+    				valoresCostoPrecioInvalidos());
+    	});
+    	
+    	fieldPrecio.textProperty().addListener((observable,oldText,newText)->{
+    		btnGuardar.setDisable(fieldNombre.getText().trim().isEmpty() ||
+    				valoresCostoPrecioInvalidos());
+    	});
+    	
+    }
+    
+    @FXML
+    private void onGuardar(ActionEvent event) {
+    	if(producto == null)
+    		return;
+    	manager.presentacion().ifPresent(dao -> {
+    		presentacion = new Presentacion(producto.getId(), 
+    				fieldNombre.getText().trim().toUpperCase(), 
+    				new BigDecimal(fieldPrecio.getText().trim()), 
+    				new BigDecimal(fieldCosto.getText().trim()));
+    		try {
+				int id = dao.insert(presentacion);
+				presentacion.setId(id);
+				btnGuardar.getScene().getWindow().hide();
+			} catch (SQLException e) {
+				presentacion = null;
+				Main.alertError("ERROR", "Ocurrió un error al guardar la presentación", e.getMessage());
+				fieldNombre.selectAll();
+				fieldNombre.requestFocus();
+				e.printStackTrace();
+			}
+    	});
+    	
+    }
+    
+    @FXML
+    private void onCancelar(ActionEvent event) {
+    	btnGuardar.getScene().getWindow().hide();
+    }
+
+}
